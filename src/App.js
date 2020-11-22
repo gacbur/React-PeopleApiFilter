6fbf2fc60c33d277170a6a3fe516cac90489d15d
Kacper <kacper0775@gmail.com>
2020-11-22 19:43:12 +0100

    add final version/ready for improvements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Modal from './components/Modal'
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([])
+  const [openModal, setOpenModal] = useState(false)
+  const [currentInspectedUser, setCurrentInspectedUser] = useState()
+  const [searchNumber, setSearchNumber] = useState(1)
+  const [enterSearchNumber, setEnterSearchNumber] = useState(false)
+
+  const handleGetUsersData = async (searchNumber) => {
+    const API = `https://randomuser.me/api/?results=${searchNumber}`
+
+    const response = await fetch(API)
+    if (response.ok) {
+      console.log("połączono prawidłowo")
+      const responseJson = await response.json()
+      setUsers(responseJson.results)
+      console.log(users)
+    }
+    else {
+      console.log("połączenie nie przebiegło prawidłowo")
+    }
+  }
+
+  useEffect(() => {
+    handleGetUsersData(searchNumber)
+  }, [enterSearchNumber])
+
+  const handleShowModal = (user) => {
+    setOpenModal(true)
+    setCurrentInspectedUser(user)
+  }
+
+  const handleShowUsers = (e) => {
+    e.preventDefault()
+    setEnterSearchNumber(prevState => !prevState)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-cnt">
+      <div className="search-section">
+        <h5>Enter how many users to display: (max 50!)</h5>
+        <form onSubmit={handleShowUsers}>
+          <input
+            min="1"
+            max="50"
+            type="number"
+            placeholder="Users number"
+            value={searchNumber}
+            onChange={(e) => setSearchNumber(e.target.value)}
+            required
+          ></input>
+          <button>Enter</button>
+        </form>
+      </div>
+      <div className="display-section">
+        {users.map(user => (
+          <>
+            {openModal ? <Modal currentInspectedUser={currentInspectedUser} modalOff={() => setOpenModal(false)} user={user} /> : null}
+            <div className="user-card" key={user.login.uuid}>
+              <div className="user-profile-pic">
+                <img src={user.picture.large} alt="person"></img>
+              </div>
+              <div className="user-content">
+                <p>Name: <strong>{user.name.first}</strong></p>
+                <p>Surname: <strong>{user.name.last}</strong></p>
+                <p>Age: <strong>{user.dob.age}</strong></p>
+              </div>
+              <button onClick={() => handleShowModal(user)} className="user-btn">See details</button>
+            </div>
+          </>
+        ))}
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const MODAL_STYLES = {
+    borderRadius: 12,
+    padding: '1.5em',
+    width: 400,
+    height: 450,
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#FFF',
+    zIndex: 1000
+}
+
+const OVERLAY_STYLES = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, .3)',
+    zIndex: 1000
+}
+
+const Modal = ({ modalOff, currentInspectedUser }) => {
+    return ReactDom.createPortal(
+        <>
+            <div className="modal-control">
+                <div style={OVERLAY_STYLES}>
+                    <button onClick={modalOff} className="overlay-btn">✖</button>
+                </div >
+                <div style={MODAL_STYLES}>
+                    <h3>Personal Informations:</h3>
+                    <br />
+                    <p>Title: <strong>{currentInspectedUser.name.title}</strong></p>
+                    <p>Name: <strong>{currentInspectedUser.name.first}</strong></p>
+                    <p>Surname: <strong>{currentInspectedUser.name.last}</strong></p>
+                    <br /><br />
+                    <h3>Address:</h3>
+                    <br />
+                    <p>Country: <strong>{currentInspectedUser.location.country}</strong></p>
+                    <p>City: <strong>{currentInspectedUser.location.city}</strong></p>
+                    <p>Street: <strong>{currentInspectedUser.location.street.name}/{currentInspectedUser.location.street.number}</strong></p>
+                    <p>Postal Code: <strong>{currentInspectedUser.location.postcode}</strong></p>
+                    <br /><br />
+                    <h3>Contact:</h3>
+                    <br />
+                    <p>Email: <strong>{currentInspectedUser.email}</strong></p>
+                    <p>Phone: <strong>{currentInspectedUser.phone}</strong></p>
+                </div >
+            </div>
+        </>,
+        document.getElementById('portal')
+    );
+}
+
+export default Modal;
